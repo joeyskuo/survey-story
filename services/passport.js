a8b19c487086fae307a946dc8e942c6cdd4d247a
Unknown <joeyskuo@gmail.com>
2017-08-29 14:29:50 -0700

    project structure refactor

---
+++ services/passport.js
@@ -0,0 +1,19 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+
+passport.use(new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        (accessToken, refreshToken, profile, done) => {
+        console.log('access token\n\t', accessToken);
+        console.log('refresh token\n\t', refreshToken);
+        console.log('profile\n', profile);
+
+        }
+    )
+);
+
