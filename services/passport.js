45f528bf279b1121fa9b1b478ffd2522a58ea836
Unknown <joeyskuo@gmail.com>
2017-08-29 20:15:25 -0700

    implemented serializeUser and deserializeUser methods

--- services/passport.js
+++ services/passport.js
@@ -6,8 +6,19 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
 
-passport.use(new GoogleStrategy(
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+    });
+});
+
+passport.use(
+    new GoogleStrategy(
         {
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
