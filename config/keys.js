6ea08cbab013aa51f3c6f5ee979912c14bfda5fc
Unknown <joeyskuo@gmail.com>
2017-08-29 21:28:43 -0700

    configured dev and prod keys

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+//keys.js - determine set of credentials to return
+
+if (process.env.NODE_ENV === 'production') {
+    // we are in production - return prod set of keys
+    module.exports = require('./prod');
+
+} else {
+    // we are in development - return dev keys
+    module.exports = require('./dev');
+
+}
