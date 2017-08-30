3bde685661b073f7eba2252cefeeced6a59983b4
Unknown <joeyskuo@gmail.com>
2017-08-29 20:22:32 -0700

    enabled cookies

--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const passport = require('passport');
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
