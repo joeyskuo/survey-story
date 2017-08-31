a5e5158b6ea4a86d65f550788109f2b407f44b20
Unknown <joeyskuo@gmail.com>
2017-08-30 22:36:48 -0700

    working Login and Logout routing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 class Header extends Component {
+
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li><a href="/auth/google">Login with Google</a></li>
+                );
+            default:
+                return (
+                    <li><a href="/api/logout">Logout</a></li>
+                );
+        }
+    }
+
     render() {
-        return(
+        return (
             <nav>
                 <div className="nav-wrapper">
                     <a className="left brand-logo" href="#">Survey Stork</a>
                     <ul className="right">
-                        <li>
-                            <a href="/auth/google">Login with Google</a>
-                        </li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
@@ -17,4 +32,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps({auth}) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
