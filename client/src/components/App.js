d3c11eb777a63895eaf2f940aef4801cb2876309
Unknown <joeyskuo@gmail.com>
2017-08-30 20:17:45 -0700

    implemented Materialize styling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,21 @@
-
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-
+import {BrowserRouter, Route} from 'react-router-dom';
+import Header from './Header';
 
 // Dummy components
-const Header = () => <h2>Header</h2>
-const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path='/' component={Landing}/>
+                    <Header />
+                    <Route exact path='/' component={Landing}/>
+                    <Route exact path='/surveys' component={Dashboard}/>
+                    <Route path='/surveys/new' component={SurveyNew}/>
                 </div>
             </BrowserRouter>
         </div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import 'materialize-css/dist/css/materialize.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from "./components/App";
+import App from './components/App';
 import reducers from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware());
