bc4c77eca1c5b60c6de21f6bf9c58b58811356a5
Unknown <joeyskuo@gmail.com>
2017-09-01 16:14:42 -0700

    created sendGrid Mailer class

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
-
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
@@ -17,5 +18,9 @@ module.exports = app => {
             _user: req.user.id,
             dateSent: Date.now()
         });
+
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+
+
     });
 };
---
+++ services/Mailer.js
@@ -0,0 +1,35 @@
+const sendgrid = require('sendgrid');
+const helper = sendgrid.mail;
+const keys = require('../config/keys');
+
+class Mailer extends helper.Mail {
+    constructor({subject, recipients }, content){
+        super();
+
+        this.from_email = new helper.Email('no-reply@survey-story.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+
+        this.addContent(this.body);
+        this.addClickTracking();
+    }
+
+    formatAddresses(recipients) {
+        return recipients.map(({ email }) => {
+            return new helper.Email(email);
+        });
+    }
+
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking(true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+        this.addRecipients();
+    }
+
+}
+
+module.exports = Mailer;
