c1775988e55a891bce3d15101f2d2974d811cb0c
Unknown <joeyskuo@gmail.com>
2017-08-31 18:19:42 -0700

    added payment Action and component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-
+import Payments from './Payments';
 
 class Header extends Component {
 
@@ -14,9 +14,10 @@ class Header extends Component {
                     <li><a href="/auth/google">Login with Google</a></li>
                 );
             default:
-                return (
-                    <li><a href="/api/logout">Logout</a></li>
-                );
+                return [
+                    <li key="1"><Payments /></li>,
+                    <li key="2"><a href="/api/logout">Logout</a></li>
+                ];
         }
     }
 
