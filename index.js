f1969cbf6ef3b5e9750d520bc15f0cb590b04f19
Unknown <joeyskuo@gmail.com>
2017-08-31 20:49:25 -0700

    added billing routes and middleware for login check

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const keys = require('./config/keys');
 require('./models/User');
@@ -9,6 +10,8 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
@@ -20,6 +23,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
     // Serve production assets
