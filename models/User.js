4a2728de680a6a572130957da8f512412558b074
Unknown <joeyskuo@gmail.com>
2017-08-31 23:13:45 -0700

    Users start with 1 free credit

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const userSchema = new Schema ({
    googleId: String,
    name: String,
-   credits: { type: Number, default: 0 }
+   credits: { type: Number, default: 1 }
 });
 
 mongoose.model('users', userSchema);
