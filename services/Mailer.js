8d9855f90033c1eff8db2eac49e1a3650c752f53
Unknown <joeyskuo@gmail.com>
2017-09-01 16:42:13 -0700

    added more methods from sendGrid API

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,6 +6,7 @@ class Mailer extends helper.Mail {
     constructor({subject, recipients }, content){
         super();
 
+        this.sgApi = sendgrid(keys.sendGridKey);
         this.from_email = new helper.Email('no-reply@survey-story.com');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
@@ -28,6 +29,27 @@ class Mailer extends helper.Mail {
         trackingSettings.setClickTracking(clickTracking);
         this.addTrackingSettings(trackingSettings);
         this.addRecipients();
+    }
+
+    addRecipients() {
+        const personalize = new helper.Personalization();
+
+        this.recipients.forEach(recipient => {
+           personalize.addTo(recipient);
+        });
+        this.addPersonalization(personalize);
+    }
+
+    async send() {
+        const request = this.sgApi.emptyRequest({
+           method: 'POST',
+           path: '/v3/mail/send',
+           body: this.toJSON()
+        });
+
+        
+        let response = this.sgApi.API(request);
+        return response;
     }
 
 }
