3728b5df4476163e544609cd873b88a57f42de44
Unknown <joeyskuo@gmail.com>
2017-09-02 00:43:21 -0700

    added validation rules

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -17,16 +17,14 @@ class App extends Component {
 
     render() {
         return (
-            <div className="container">
                 <BrowserRouter>
-                    <div>
+                    <div className="container">
                         <Header />
                         <Route exact path='/' component={Landing}/>
                         <Route exact path='/surveys' component={Dashboard}/>
                         <Route path='/surveys/new' component={SurveyNew}/>
                     </div>
                 </BrowserRouter>
-            </div>
         );
     }
 };
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 
 const FIELDS = [
@@ -30,13 +31,31 @@ class SurveyForm extends Component {
             <div>
                 <form onSubmit={this.props.handleSubmit(values => console.log((values)))}>
                     {this.renderFields()}
-                    <button type="submit">Submit</button>
+                    <Link to="/surveys" className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button type="submit" className="teal btn-flat right white-text">
+                        Next
+                    </button>
                 </form>
             </div>
         );
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    _.each(FIELDS, ({ name }) => {
+        if(!values[name]) {
+            errors[name] = `You must provide a value`;
+        }
+    });
+
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'surveyForm'
 })(SurveyForm);
